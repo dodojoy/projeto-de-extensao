0967242278ca50f2e8c2159cdcfb5eeafcb4659e
Joyce <joyce.mbt@gmail.com>
2024-05-26 21:24:20 -0300

    feat: implement menu navigation

--- src/components/homepage/Header.jsx
+++ src/components/homepage/Header.jsx
@@ -2,35 +2,61 @@ import { useEffect, useState } from "react";
 import MenuHeader from "./MenuHeader";
 import styles from "@/styles/Header.module.css";
 import containerStyles from "@/styles/Container.module.css";
+import { useRouter } from "next/router";
 
-export default function Header() {
+export default function Header({ careersRef, aboutRef }) {
   const [menuIsOpen, setMenuIsOpen] = useState(false);
   const [isDesktop, setIsDesktop] = useState(false);
-  
+  const router = useRouter();
+
   const handleMenu = () => {
     setMenuIsOpen(!menuIsOpen);
   };
 
+  const scrollToRef = (ref) => {
+    if (ref && ref.current) {
+      ref.current.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  const handleNavigation = (section) => {
+    if (router.pathname === "/") {
+      if (section === "home") {
+        // Redirecionar para a página inicial
+        router.push("/");
+      } else if (section === "careers" && careersRef && careersRef.current) {
+        // Rolagem suave até a seção de carreiras
+        scrollToRef(careersRef);
+      } else if (section === "about" && aboutRef && aboutRef.current) {
+        // Rolagem suave até a seção "Sobre"
+        scrollToRef(aboutRef);
+      }
+    } else {
+      window.location.href = `/#${section}`;
+    }
+
+    // Fechar o menu se estiver aberto
+    if (menuIsOpen) {
+      handleMenu();
+    }
+  };
+
   useEffect(() => {
-    // Função para atualizar o estado isDesktop com base no tamanho da janela
     const updateMedia = () => {
-      setIsDesktop(window.innerWidth > 768); // Você pode ajustar o valor do breakpoint conforme necessário
+      setIsDesktop(window.innerWidth > 768);
     };
 
-    // Adicionar listener para o evento resize
     window.addEventListener("resize", updateMedia);
 
-    // Chamar a função de atualização uma vez ao carregar o componente
     updateMedia();
 
-    // Remover o listener ao desmontar o componente
     return () => window.removeEventListener("resize", updateMedia);
   }, []);
 
   return (
     <header className={`${styles.header}`}>
-       <div className={`${styles.header} ${containerStyles.container}`}>
-        <img src="/assets/logo.svg" alt="logo icon" />
+      <div className={`${styles.header} ${containerStyles.container}`}>
+        <img src="/assets/logo.svg" alt="logo icon" onClick={() => router.push("/")} />
         {!isDesktop && (
           <img
             src="/assets/menuHamburguer.svg"
@@ -41,14 +67,14 @@ export default function Header() {
         {isDesktop && (
           <nav>
             <ul className={styles.desktopMenu}>
-              <li>home</li>
-              <li>carreiras</li>
-              <li>sobre</li>
+              <li onClick={() => router.push("/")}>home</li>
+              <li onClick={() => handleNavigation("careers")}>carreiras</li>
+              <li onClick={() => handleNavigation("about")}>sobre</li>
             </ul>
           </nav>
         )}
       </div>
-      {menuIsOpen && !isDesktop && <MenuHeader handleMenu={handleMenu} />}
+      {menuIsOpen && !isDesktop && <MenuHeader handleMenu={handleMenu} aboutRef={aboutRef} careersRef={careersRef} />}
     </header>
   );
 }
