3e26456de89f097e8c4fa0cc3e97e18c53b08038
Joyce <joyce.mbt@gmail.com>
2024-05-31 17:47:34 -0300

    fix build errors

--- src/pages/manutencao-de-hardware.js
+++ src/pages/manutencao-de-hardware.js
@@ -2,19 +2,19 @@ import MaterialManutencao from "@/components/manutencao-de-hardware/Material";
 
 import Footer from "@/components/homepage/Footer";
 import Header from "@/components/homepage/Header";
-import MainSection from "@/components/manutencao/MainSection";
-import DevFunctionSection from "@/components/manutencao/DevFunctionContent";
+import MainSection from "@/components/manutencao-de-hardware/MainSection";
+import DevFunctionSection from "@/components/manutencao-de-hardware/DevFunctionContent";
 
 export default function Manutencao() {
   return <div>Manutenção de Hardware</div>;
-  
+
   return (
     <>
       <Header />
       <main>
         <MainSection />
         <DevFunctionSection />
-        <MaterialManutencao/>
+        <MaterialManutencao />
       </main>
       <Footer />
     </>
